2a283c0bfd5a1abba94602df4228f21906a91d8a
mentosmenno2 <mentosmenno2@hotmail.com>
2025-03-09 21:44:01 +0100

    add bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+// Define constants
+
+use Mentosmenno2\MaartenBday2025\App;
+
+define('MAARTEN_BDAY_2025_NAMESPACE', 'Mentosmenno2\\MaartenBday2025\\');
+define('MAARTEN_BDAY_2025_ROOT_DIR', __DIR__);
+define('MAARTEN_BDAY_2025_CLI', php_sapi_name() === 'cli');
+
+// Autoload
+require_once __DIR__ . '/autoloader.php';
+
+$app = App::getInstance();
