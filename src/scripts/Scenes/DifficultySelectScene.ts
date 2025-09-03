d62cfdd93f9dc16e30f6ad9768aa161fcef12d08
menno-ll <menno@level-level.com>
2025-09-03 22:40:08 +0200

    choose difficulties

--- src/scripts/Scenes/DifficultySelectScene.ts
+++ src/scripts/Scenes/DifficultySelectScene.ts
@@ -9,16 +9,86 @@ import { LevelScene } from './LevelScene.js';
 export class DifficultySelectScene extends AbstractScene {
 
 	private backgroundImage: HTMLImageElement|null;
-	private coverImage: HTMLImageElement|null;
+	private coverImage: {
+		x: number;
+		y: number;
+		w: number;
+		h: number;
+		image: HTMLImageElement|null;
+	};
+
+	private songTitle: {
+		x: number;
+		y: number;
+		fontSize: number;
+		text: string;
+		maxWidth: number;
+	};
+
+	private songArtist: {
+		x: number;
+		y: number;
+		fontSize: number;
+		text: string;
+		maxWidth: number;
+	};
+
+	private mapper: {
+		x: number;
+		y: number;
+		fontSize: number;
+		text: string|null;
+		maxWidth: number;
+	};
+
+	private chooseText: {
+		x: number;
+		y: number;
+		fontSize: number;
+		text: string;
+		maxWidth: number;
+	};
 
 	private difficultyButtons: Array<{
-		x: number; y: number; w: number; h: number; difficultyIndex: number, difficultyName: string
+		x: number; y: number; w: number; h: number; difficultyIndex: number, text: string
 	}>;
 
 	private previewAudio: HTMLAudioElement;
 
 	constructor(game: Game, private song: SongInterface) {
 		super(game);
+
+		this.songTitle = {
+			x: 0,
+			y: 0,
+			fontSize: 0,
+			text: this.song.title,
+			maxWidth: 0
+		};
+
+		this.songArtist = {
+			x: 0,
+			y: 0,
+			fontSize: 0,
+			text: this.song.artist,
+			maxWidth: 0
+		};
+
+		this.mapper = {
+			x: 0,
+			y: 0,
+			fontSize: 0,
+			text: `Maker: ${this.song.builder || 'Onbekend'}`,
+			maxWidth: 0
+		};
+
+		this.chooseText = {
+			x: 0,
+			y: 0,
+			fontSize: 0,
+			text: 'Kies moeilijkheidsgraad',
+			maxWidth: 0
+		};
 
 		this.difficultyButtons = [];
 		for (let difficultyIndex = 0; difficultyIndex < this.song.difficulties.length; difficultyIndex++) {
@@ -28,7 +98,7 @@ export class DifficultySelectScene extends AbstractScene {
 				w: 0,
 				h: 0,
 				difficultyIndex: difficultyIndex,
-				difficultyName: this.song.difficulties[difficultyIndex].name
+				text: this.song.difficulties[difficultyIndex].name
 			});
 		}
 
@@ -39,10 +109,17 @@ export class DifficultySelectScene extends AbstractScene {
 			this.backgroundImage.src = backgroundImageBase64;
 		}
 
+		this.coverImage = {
+			x: 0,
+			y: 0,
+			w: 0,
+			h: 0,
+			image: null
+		};
 		const coverImageBase64 = this.song.coverImageBase64 || this.song.backgroundImageBase64;
 		if (coverImageBase64) {
-			this.coverImage = new Image();
-			this.coverImage.src = coverImageBase64;
+			this.coverImage.image = new Image();
+			this.coverImage.image.src = coverImageBase64;
 		}
 
 		this.previewAudio = document.createElement('audio');
@@ -59,18 +136,71 @@ export class DifficultySelectScene extends AbstractScene {
 	}
 
 	public update(): void {
+		this.updateCoverImage();
+		this.updateSongTitle();
+		this.updateSongArtist();
+		this.updateMapper();
+		this.updateChooseText();
 		this.updateDifficultyButtons();
 		this.handleDifficultyButtonsClick();
+	}
+
+	private updateCoverImage(): void {
+		const { width, height } = this.game.getCanvas().getElement();
+
+		const size = Math.round(Math.min(width, height) * 0.15);
+
+		this.coverImage.x = 20;
+		this.coverImage.y = 20;
+		this.coverImage.w = size;
+		this.coverImage.h = size;
+	}
+
+	private updateSongTitle(): void {
+		const { width } = this.game.getCanvas().getElement();
+
+		this.songTitle.x = this.coverImage.x + this.coverImage.w + 20;
+		this.songTitle.y = this.coverImage.y;
+		this.songTitle.fontSize = this.coverImage.h * 0.3;
+		this.songTitle.maxWidth = Math.round( width - this.songTitle.x - 20 );
+	}
+
+	private updateSongArtist(): void {
+		const { width } = this.game.getCanvas().getElement();
+
+		this.songArtist.x = this.coverImage.x + this.coverImage.w + 20;
+		this.songArtist.y = this.coverImage.y + this.coverImage.h * 0.35;
+		this.songArtist.fontSize = this.coverImage.h * 0.3;
+		this.songArtist.maxWidth = Math.round( width - this.songArtist.x - 20 );
+	}
+
+	private updateMapper(): void {
+		const { width } = this.game.getCanvas().getElement();
+
+		this.mapper.x = this.coverImage.x + this.coverImage.w + 20;
+		this.mapper.y = this.coverImage.y + this.coverImage.h * 0.7;
+		this.mapper.fontSize = this.coverImage.h * 0.25;
+		this.mapper.maxWidth = Math.round( width - this.mapper.x - 20 );
+	}
+
+	private updateChooseText(): void {
+		const { width, height } = this.game.getCanvas().getElement();
+
+		this.chooseText.x = width / 2;
+		this.chooseText.y = this.coverImage.y + this.coverImage.h + 20;
+		this.chooseText.fontSize = Math.min(width, height) * 0.05;
+		this.chooseText.maxWidth = Math.round( width - 40 );
 	}
 
 	private updateDifficultyButtons(): void {
 		const { width, height } = this.game.getCanvas().getElement();
 
+		const topY = this.chooseText.y + this.chooseText.fontSize + 20;
 		this.difficultyButtons.forEach((button, index) => {
-			button.x = Math.round((width - button.w) / 2);
-			button.y = Math.round(height * (0.3 + index * 0.1));
-			button.w = Math.round(width * 0.28);
-			button.h = Math.round(height * 0.08);
+			button.x = 20;
+			button.y = topY + ( index * ( height * 0.05 + 20 ) );
+			button.w = width - 40;
+			button.h = height * 0.05;
 		});
 	}
 
@@ -100,6 +230,7 @@ export class DifficultySelectScene extends AbstractScene {
 		this.renderSongTitle(ctx);
 		this.renderSongArtist(ctx);
 		this.renderMapper(ctx);
+		this.renderChooseText(ctx);
 		this.renderDifficultyButtons(ctx);
 	}
 
@@ -133,93 +264,93 @@ export class DifficultySelectScene extends AbstractScene {
 	}
 
 	private renderCoverImage(ctx: CanvasRenderingContext2D): void {
-		if (! this.coverImage) {
+		if (! this.coverImage.image) {
 			return;
 		}
 
-		const { width, height } = ctx.canvas;
-
 		ctx.save();
-		const artSize = Math.round(Math.min(width, height) * 0.25);
-		const artX = width / 2 - artSize / 2;
-		const artY = Math.round(height * 0.12);
 		ctx.beginPath();
-		ctx.arc(width/2, artY + artSize/2, artSize/2, 0, Math.PI*2);
+		ctx.arc(
+			this.coverImage.x + this.coverImage.w / 2,
+			this.coverImage.y + this.coverImage.h / 2,
+			this.coverImage.h / 2,
+			0,
+			Math.PI * 2
+		);
 		ctx.closePath();
 		ctx.clip();
 
-		const imageAspectRatio = this.coverImage.width / this.coverImage.height;
-		const artAspectRatio = artSize / artSize;
+		const imageAspectRatio = this.coverImage.image.width / this.coverImage.image.height;
+		const artAspectRatio = this.coverImage.w / this.coverImage.h;
 		if (imageAspectRatio > artAspectRatio) {
-			const scaledHeight = artSize;
+			const scaledHeight = this.coverImage.h;
 			const scaledWidth = scaledHeight * imageAspectRatio;
-			ctx.drawImage(this.coverImage, artX + (artSize - scaledWidth) / 2, artY, scaledWidth, scaledHeight);
+			ctx.drawImage(this.coverImage.image, this.coverImage.x + (this.coverImage.w - scaledWidth) / 2, this.coverImage.y, scaledWidth, scaledHeight);
 		} else {
-			const scaledWidth = artSize;
+			const scaledWidth = this.coverImage.w;
 			const scaledHeight = scaledWidth / imageAspectRatio;
-			ctx.drawImage(this.coverImage, artX, artY + (artSize - scaledHeight) / 2, scaledWidth, scaledHeight);
+			ctx.drawImage(this.coverImage.image, this.coverImage.x, this.coverImage.y + (this.coverImage.h - scaledHeight) / 2, scaledWidth, scaledHeight);
 		}
 		ctx.restore();
 	}
 
 	private renderSongTitle(ctx: CanvasRenderingContext2D): void {
-		const { width, height } = ctx.canvas;
-
 		ctx.save();
-		let titleFontSize = Math.round(height*0.06);
-		ctx.font = `${titleFontSize}px Arial`;
-		let titleWidth = ctx.measureText(this.song.title).width;
-		while (titleWidth > width * 0.92 && titleFontSize > 10) {
-			titleFontSize--;
-			ctx.font = `${titleFontSize}px Arial`;
-			titleWidth = ctx.measureText(this.song.title).width;
-		}
+		ctx.font = `${this.songTitle.fontSize}px Arial`;
 		ctx.fillStyle = ColorUtils.getHex(ColorEnum.White);
-		ctx.textAlign = 'center';
+		ctx.textAlign = 'left';
 		ctx.textBaseline = 'top';
 		ctx.strokeStyle = ColorUtils.getHex(ColorEnum.DarkBlue);
 		ctx.lineWidth = 5;
-		ctx.strokeText(this.song.title, width/2, height*0.42);
-		ctx.fillText(this.song.title, width/2, height*0.42);
+
+		ctx.strokeText(this.songTitle.text, this.songTitle.x, this.songTitle.y, this.songTitle.maxWidth);
+		ctx.fillText(this.songTitle.text, this.songTitle.x, this.songTitle.y, this.songTitle.maxWidth);
 		ctx.restore();
 	}
 
 	private renderSongArtist(ctx: CanvasRenderingContext2D): void {
-		const { width, height } = ctx.canvas;
+		ctx.save();
+		ctx.font = `${this.songArtist.fontSize}px Arial`;
+		ctx.fillStyle = ColorUtils.getHex(ColorEnum.White);
+		ctx.textAlign = 'left';
+		ctx.textBaseline = 'top';
+		ctx.strokeStyle = ColorUtils.getHex(ColorEnum.DarkBlue);
+		ctx.lineWidth = 5;
+
+		ctx.strokeText(this.songArtist.text, this.songArtist.x, this.songArtist.y, this.songArtist.maxWidth);
+		ctx.fillText(this.songArtist.text, this.songArtist.x, this.songArtist.y, this.songArtist.maxWidth);
+		ctx.restore();
+	}
+
+	private renderMapper(ctx: CanvasRenderingContext2D): void {
+		if (! this.mapper.text) {
+			return;
+		}
 
 		ctx.save();
-		let artistFontSize = Math.round(height*0.04);
-		ctx.font = `${artistFontSize}px Arial`;
-		let artistWidth = ctx.measureText(this.song.artist).width;
-		while (artistWidth > width * 0.92 && artistFontSize > 8) {
-			artistFontSize--;
-			ctx.font = `${artistFontSize}px Arial`;
-			artistWidth = ctx.measureText(this.song.artist).width;
-		}
+		ctx.font = `${this.mapper.fontSize}px Arial`;
+		ctx.fillStyle = ColorUtils.getHex(ColorEnum.White);
+		ctx.textAlign = 'left';
+		ctx.textBaseline = 'top';
+		ctx.strokeStyle = ColorUtils.getHex(ColorEnum.DarkBlue);
+		ctx.lineWidth = 5;
+
+		ctx.strokeText(this.mapper.text, this.mapper.x, this.mapper.y, this.mapper.maxWidth);
+		ctx.fillText(this.mapper.text, this.mapper.x, this.mapper.y, this.mapper.maxWidth);
+		ctx.restore();
+	}
+
+	private renderChooseText(ctx: CanvasRenderingContext2D): void {
+		ctx.save();
+		ctx.font = `${this.chooseText.fontSize}px Arial`;
 		ctx.fillStyle = ColorUtils.getHex(ColorEnum.White);
 		ctx.textAlign = 'center';
 		ctx.textBaseline = 'top';
 		ctx.strokeStyle = ColorUtils.getHex(ColorEnum.DarkBlue);
 		ctx.lineWidth = 5;
-		ctx.strokeText(this.song.artist, width/2, height*0.50);
-		ctx.fillText(this.song.artist, width/2, height*0.50);
-		ctx.restore();
-	}
 
-	private renderMapper(ctx: CanvasRenderingContext2D): void {
-		const { width, height } = ctx.canvas;
-
-		ctx.save();
-		if (this.song.builder) {
-			ctx.font = `${Math.round(height*0.025)}px Arial`;
-			ctx.fillStyle = ColorUtils.getHex(ColorEnum.LightBlue);
-			ctx.textAlign = 'center';
-			ctx.textBaseline = 'top';
-			ctx.strokeStyle = ColorUtils.getHex(ColorEnum.DarkBlue);
-			ctx.lineWidth = 5;
-			ctx.strokeText(`Maker: ${this.song.builder}`, width/2, height*0.55);
-			ctx.fillText(`Maker: ${this.song.builder}`, width/2, height*0.55);
-		}
+		ctx.strokeText(this.chooseText.text, this.chooseText.x, this.chooseText.y, this.chooseText.maxWidth);
+		ctx.fillText(this.chooseText.text, this.chooseText.x, this.chooseText.y, this.chooseText.maxWidth);
 		ctx.restore();
 	}
 
@@ -239,7 +370,7 @@ export class DifficultySelectScene extends AbstractScene {
 			ctx.fillStyle = ColorUtils.getHex(ColorEnum.White);
 			ctx.textAlign = 'center';
 			ctx.textBaseline = 'middle';
-			ctx.fillText(difficultyButton.difficultyName, difficultyButton.x + difficultyButton.w / 2, difficultyButton.y + difficultyButton.h / 2);
+			ctx.fillText(difficultyButton.text, difficultyButton.x + difficultyButton.w / 2, difficultyButton.y + difficultyButton.h / 2, difficultyButton.w);
 		}
 		ctx.restore();
 	}
